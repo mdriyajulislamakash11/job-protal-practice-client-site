589c28ea4257ee507b73039e6549f9a2bd95ed39
Akash <hafejmohammadakash@gmail.com>
2025-07-02 13:54:35 +0600

    application count er kaj korlam

--- src/pages/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../auth/AuthProvider";
 
 const MyPostedJobs = () => {
-  return (
-    <div>MyPostedJobs</div>
-  )
-}
+  const [jobs, setJobs] = useState([]);
+  const { user } = useContext(AuthContext);
 
-export default MyPostedJobs
+  console.log(jobs)
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/jobs?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setJobs(data);
+      });
+  }, [user?.email]);
+
+  return (
+    <div className="m-20">
+      
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Application Count</th>
+              <th>Applications</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {jobs.map((job, index) => (
+              <tr key={index} className="bg-base-200 ">
+                <th>{index + 1}</th>
+                <td>{job.title}</td>
+                <td>{job.applicationDeadline}</td>
+                <td>{job.applicationCount}</td>
+                <td>
+                  <p className="text-blue-800 font-bold">View Applications</p>
+                </td>
+                <td>
+                  <button className="btn px-10">E</button>
+                  <button className="btn px-10">X</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default MyPostedJobs;
