b717d7e7f1a534005e7382765ee565bcc8100b14
Akash <hafejmohammadakash@gmail.com>
2025-07-02 10:28:41 +0600

    private router er kaj korlamn

--- src/pages/MyApplication.jsx
+++ src/pages/MyApplication.jsx
@@ -1,26 +1,26 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../auth/AuthProvider';
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../auth/AuthProvider";
 
 const MyApplication = () => {
+  const [jobs, setJobs] = useState([]);
+  const { user } = useContext(AuthContext);
 
-    const [jobs, setJobs] = useState([])
-    const {user} = useContext(AuthContext)
-    
-    // 
-    useEffect(() => {
-        fetch(`http://localhost:5000/job-Application?email=${user.email}`)
-        .then(res => res.json())
-        .then(data => {
-            setJobs(data)
-        })
-    }, [user.email])
+  //
+  useEffect(() => {
+    fetch(`http://localhost:5000/job-Application?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setJobs(data);
+      });
+  }, [user?.email]);
 
+  return (
+    <div>
+      <p>my applications: {jobs.length}</p>
 
-    return (
-        <div>
-            <p>my applications: {jobs.length}</p>
-        </div>
-    );
+      
+    </div>
+  );
 };
 
 export default MyApplication;
