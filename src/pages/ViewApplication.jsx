2b6e21ad8485b224057aea1a12ea1b7b73e651ca
Akash <hafejmohammadakash@gmail.com>
2025-07-02 23:22:40 +0600

    ami ekhane status update er kaj kortesi

--- src/pages/ViewApplication.jsx
+++ src/pages/ViewApplication.jsx
@@ -1,15 +1,82 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ViewApplication = () => {
-const application = useLoaderData()
-    console.log(application)
+  const application = useLoaderData();
+  console.log(application);
 
-    return (
-        <div>
-            
-        </div>
-    );
+  const handleStatusUpdate = (e, id) => {
+
+    const data = {
+      status: e.target.value,
+    };
+
+    fetch(`https://jobprotal-delta.vercel.app/job_applications/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Status Has been updated.",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
+  return (
+    <div className="m-10">
+      <h2 className="text-3xl mb-8 text-center">
+        Applications for this job: {application.length}
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Email</th>
+              <th>Status</th>
+              <th>GitHub URL</th>
+              <th>Update Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {application.map((app, index) => (
+              <tr key={app._id}>
+                <th>{index + 1}</th>
+                <td>{app.applicant_email}</td>
+                <td>Quality Control Specialist</td>
+                <td>{app.github}</td>
+                <td>
+                  <select
+                    onChange={(e) => handleStatusUpdate(e, app._id)}
+                    defaultValue={app.status || "Change Status"}
+                    className="select select-bordered select-xs w-full max-w-xs"
+                  >
+                    <option disabled>Change Status</option>
+                    <option>Under Review</option>
+                    <option>Set Interview</option>
+                    <option>Hired</option>
+                    <option>Rejected</option>
+                  </select>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ViewApplication;
